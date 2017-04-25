113e100c1c787d5452bd2b162ac83d0468cd5f46
asWorks <x10@asWorks.de>
2017-04-26 01:46:58 +0200

    InialCommit + Initial Configuration of log4net

---
+++ ExerciseSummary.BL.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExerciseSummary.BL.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExerciseSummary.BL.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("69ba5d02-c512-423a-a401-60b84df4cf63")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ExerciseSummary.Console/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace ExerciseSummary.ConsoleApp
+{
+    class Program
+    {
+
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            log.Error("This is my error message");
+
+            log.Debug("And this is a Debug message");
+
+
+            var x = new Program();
+
+
+            x.DoSomethingElse();
+            
+            Console.ReadLine();
+        }
+
+
+
+        void DoSomethingElse()
+        {
+            Console.WriteLine("SomethingElse was called");
+
+            log.Debug("And this is the message from SomethinElse");
+
+
+        }
+    }
+}
